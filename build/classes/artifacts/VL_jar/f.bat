4fb502c4963385e0939441c9749f684ac8ecba43
PrimeLaft <PrimeLaft@gmail.com>
2015-07-27 18:13:23 +0200

    Build 0.01

---
+++ build/classes/artifacts/VL_jar/f.bat
@@ -0,0 +1,2 @@
+java -jar VL.jar
+Pause
---
+++ desktop/src/com/voxelleaf/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.voxelleaf.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.voxelleaf.game.VoxelLeaf;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new VoxelLeaf(), config);
+	}
+}
