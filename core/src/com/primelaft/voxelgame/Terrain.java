809177e04f3c41ef24cb9bb55925c00affd75668
PrimeLaft <PrimeLaft@gmail.com>
2015-07-28 15:06:21 +0200

    Map is now generating in 3 dimensions (x, y, z)
    Map is now generating in 3 dimensions (x, y, z)

--- core/src/com/primelaft/voxelgame/Terrain.java
+++ core/src/com/primelaft/voxelgame/Terrain.java
@@ -18,8 +18,8 @@ import com.badlogic.gdx.math.Vector3;
 
 public class Terrain {
     public Environment environment;
-    public Model[] Blocks = new Model[200];
-    public ModelInstance[] BlocksInstances = new ModelInstance[200];
+    public Model[][][] Blocks = new Model[10][10][10];
+    public ModelInstance[][][] BlocksInstances = new ModelInstance[10][10][10];
 
     //**************************************************
     // Initialize world
@@ -38,32 +38,43 @@ public class Terrain {
         FileHandle imageFileHandle = Gdx.files.internal("grass.jpg");
 
         ModelBuilder modelBuilder = new ModelBuilder();
-        for(int i = 0; i < Blocks.length; i++)
-        {
-            Blocks[i] = modelBuilder.createBox(2f, 2f, 2f, new Material(),
-                    VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal | VertexAttributes.Usage.TextureCoordinates);
-            BlocksInstances[i] = new ModelInstance(Blocks[i]);
-            BlocksInstances[i].materials.first().set(TextureAttribute.createDiffuse(new Texture(imageFileHandle)));
+		for(int x = 0; x < Blocks.length; x++){
+			for(int z = 0; z < Blocks.length; z++)
+            {
+                for(int y = 0; y < Blocks.length; y++)
+                {
+                    Blocks[z][x][y] = modelBuilder.createBox(1f, 1f, 1f, new Material(),
+                            VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal | VertexAttributes.Usage.TextureCoordinates);
+                    BlocksInstances[z][x][y] = new ModelInstance(Blocks[z][x][y]);
+                    BlocksInstances[z][x][y].materials.first().set(TextureAttribute.createDiffuse(new Texture(imageFileHandle)));
 
-            BlocksInstances[i].transform.setToTranslation(i * 2, 0, 0f);
-        }
+                    BlocksInstances[z][x][y].transform.setToTranslation(x * 2, y * 2, z * 2);
+                }
+			}
+		}
     }
     //**************************************************
     // Real time render terrain
     //**************************************************
     public void RenderTerrain()
     {
-        for(int i = 0; i < BlocksInstances.length; i++)
+        for(int z = 0; z < BlocksInstances.length; z++)
         {
-            Vector3 vec = new Vector3();
-            if(Player.cam.position.x + 50 >= BlocksInstances[i].transform.getTranslation(vec).x &&
-                    Player.cam.position.x - 50 <= BlocksInstances[i].transform.getTranslation(vec).x &&
-                    Player.cam.position.z + 50 >= BlocksInstances[i].transform.getTranslation(vec).z &&
-                    Player.cam.position.z - 50 <= BlocksInstances[i].transform.getTranslation(vec).z &&
-                    Player.cam.position.y + 10 >= BlocksInstances[i].transform.getTranslation(vec).y &&
-                    Player.cam.position.y - 10 <= BlocksInstances[i].transform.getTranslation(vec).y)
+            for(int x = 0; x < BlocksInstances.length; x++)
             {
-                VoxelGame.modelBatch.render(BlocksInstances[i], environment);
+                for(int y = 0; y < BlocksInstances.length; y++)
+                {
+                    Vector3 vec = new Vector3();
+                    if(Player.cam.position.x + 200 >= BlocksInstances[z][x][y].transform.getTranslation(vec).x &&
+                            Player.cam.position.x - 200 <= BlocksInstances[z][x][y].transform.getTranslation(vec).x &&
+                            Player.cam.position.z + 200 >= BlocksInstances[z][x][y].transform.getTranslation(vec).z &&
+                            Player.cam.position.z - 200 <= BlocksInstances[z][x][y].transform.getTranslation(vec).z &&
+                            Player.cam.position.y + 50 >= BlocksInstances[z][x][y].transform.getTranslation(vec).y &&
+                            Player.cam.position.y - 50 <= BlocksInstances[z][x][y].transform.getTranslation(vec).y)
+                    {
+                        VoxelGame.modelBatch.render(BlocksInstances[z][x][y], environment);
+                    }
+                }
             }
         }
     }
